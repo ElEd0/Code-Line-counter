ea0178b872f959649f52d75aa9a4cd1d0f3321dd
ElEd0 <edoelgamer@gmail.com>
2019-02-22 01:03:21 +0100

    command dispatch

--- es/ed0/linecounter/LineCounter.java
+++ es/ed0/linecounter/LineCounter.java
@@ -7,14 +7,27 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+import es.ed0.linecounter.ResultTable.ResultPopulator;
+
 public class LineCounter {
 
 	public static void main(String[] args) {
 		new LineCounter();
 	}
+
+	public static final String
+		help = "help",
+		create = "create",
+		show = "show",
+		info = "info",
+		print = "print",
+		delete = "delete";
+	
+	
 	
 	private ArrayList<Profile> profiles;
-	
+	private final Scanner t;
+	private boolean profilesDirty = false;
 	
 	private LineCounter() {
 		
@@ -25,35 +38,104 @@ public class LineCounter {
 		System.out.println("|_______ \\__|___|  /\\___  >\\______  /\\____/|____/|___|  /__|  \\___  >__|   ");
 		System.out.println("        \\/       \\/     \\/        \\/                  \\/          \\/       ");
 
-		final Scanner t = new Scanner(System.in);
+		System.out.println("\nHi! Type help to print a command list\n");
+
+		profiles = Profile.getProfiles();
+		printProfiles();
+		
+		t = new Scanner(System.in);
 		
 		boolean stop = false;
 		while (!stop) {
-			profiles = Profile.getProfiles();
-			
-			System.out.println("Select a profile");
-			System.out.println("0 - new profile");
-			for (int i = 1; i <= profiles.size(); i++)
-				System.out.println(i + " - " + profiles.get(i - 1).getName());
-			
-			try {
-				int pIndex = Integer.valueOf(t.nextLine());
-				if (pIndex == 0)
-					createProfile();
-				else
-					profiles.get(pIndex - 1).printResults();
-			} catch (NumberFormatException | IndexOutOfBoundsException e) {
-				System.err.println("Invalid profile option!");
-				e.printStackTrace();
+			if (profilesDirty) {
+				profiles = Profile.getProfiles();
+				profilesDirty = false;
 			}
+			System.out.print("> ");
+			final String line = t.nextLine();
+			final String[] args = line.split(" ");			
+			System.out.println(dispatchCommand(args));
 		}
 		
 		t.close();
 		
 	}
 	
-	private void createProfile() {
-		final Scanner t = new Scanner(System.in);
+	private String dispatchCommand(String[] args) {
+		StringBuilder sb = new StringBuilder("");
+		switch (args[0]) {
+		case help:
+			sb.append("\thelp - show command list\n"
+					+ "\tcreate - create a new line counter profile\n"
+					+ "\tprint n - count lines and print results for profile number n\n"
+					+ "\tshow - show all the profiles and their identification number\n"
+					+ "\tinfo n - show info about the profile number n\n"
+					+ "\tdelete n - delete the profile number n");
+			break;
+		case create:
+			try {
+				final Profile prof = createProfile();
+				sb.append("New profile saved: " + prof.getNumber() + " -> "+ prof.getName());
+				profilesDirty = true;
+			} catch (IOException e) {
+				e.printStackTrace();
+				sb.append("Error saving profile. Reason: " + e.getLocalizedMessage());
+			}
+			break;
+		case show:
+			printProfiles();
+			break;
+		case print: case info: case delete:
+			if (args.length < 2)
+				sb.append("Syntax: \"").append(args[0]).append(" n\"  where n is the number of profile (write \"show\" to list the profiles)");
+			else {
+				try {
+					final Profile target = profiles.get(Integer.valueOf(args[1]));
+					switch (args[0]) {
+					case print: target.printResults(); break;
+					case info: target.printInfo(); break;
+					case delete:
+						try {
+							target.delete();
+							sb.append("Profile " + target.getName() + " deleted");
+							profilesDirty = true;
+						} catch (IOException e) {
+							sb.append("Error deleting profile. Reason: " + e.getLocalizedMessage());
+						}
+						break;
+					}
+				} catch (NumberFormatException e) {
+					sb.append("The value ").append(args[1]).append(" is not a valid number");
+				} catch (IndexOutOfBoundsException e) {
+					sb.append("No profile number ").append(args[1]);
+				}
+			}
+			break;
+			default:
+				sb.append("The command ").append(args[0]).append(" does not exist. Enter \"help\" for command list");
+				break;
+		}
+		return sb.append("\n").toString();
+	}
+	
+	private void printProfiles() {
+		ResultTable<Profile> projectTable = new ResultTable<Profile>(profiles, "Nº", "Name", "Project count");
+		
+		projectTable.setResultPopulator(new ResultPopulator<Profile>() {
+			@Override
+			public ArrayList<String> getViewForRow(int index, Profile entry) {
+				final ArrayList<String> data = new ArrayList<String>();
+				data.add(index + "");
+				data.add(entry.getName());
+				data.add(entry.getProjects().size() + "");
+				return data;
+			}
+		});
+		
+		projectTable.print();
+	}
+	
+	private Profile createProfile() throws IOException {
 		System.out.print(" - Profile name: ");
 		String name = t.nextLine();
 		System.out.print(" - Proyect paths (separated by commas): ");
@@ -67,10 +149,9 @@ public class LineCounter {
 		System.out.print(" - Code file extensions (separated by commas): ");
 		ArrayList<String> exts = Profile.getArrayFromCsv(t.nextLine(), ",");
 		
-		Profile prof = new Profile(name, ps, exts);
+		Profile prof = new Profile(profiles.size(), name, ps, exts);
 		prof.serialize();
-		
-		t.close();
+		return prof;
 	}
 	
 	
--- es/ed0/linecounter/Profile.java
+++ es/ed0/linecounter/Profile.java
@@ -19,15 +19,34 @@ public class Profile {
 	private final static String profiles_data = "profiles.data";
 	private final int longerFilesMaxCount = 3;
 	
+	private int number;
 	private String name;
 	private ArrayList<Project> projects;
 	private ArrayList<String> extensions;
 	
-	public Profile(String name, ArrayList<Project> projects, ArrayList<String> extensions) {
+	public Profile(int number, String name, ArrayList<Project> projects, ArrayList<String> extensions) {
+		this.number = number;
 		this.name = name;
 		this.projects = projects;
 		this.extensions = extensions;
 	}
+	
+	public void printInfo() {
+		System.out.println("========================================");
+		
+		System.out.println("Profile " + name);
+		System.out.print("\t- Extensions (" + extensions.size() + "):\t");
+		for (int i = 0; i < extensions.size(); i++)
+			System.out.print((i != 0 ? ", " : "") + extensions.get(i));
+		System.out.println();
+		
+		System.out.println("\t- Projects (" + projects.size() + "):");
+		for (Project pr : projects)
+			System.out.println("\t\t- " + pr.getPath() + "\tFiles loaded: " + pr.isExplored());
+		
+		System.out.println("========================================");
+	}
+
 	
 	public void printResults() {
 		for (Project p : projects)
@@ -75,6 +94,7 @@ public class Profile {
 			if (!f.exists())
 				f.createNewFile();
 			final BufferedReader br = new BufferedReader(new FileReader(f));
+			int counter = 0;
 			String line = null;
 			while((line = br.readLine()) != null) {
 				try {
@@ -84,10 +104,11 @@ public class Profile {
 					for (String p : getArrayFromCsv(data[1], "&"))
 						ps.add(new Project(p));
 					ArrayList<String> exts = getArrayFromCsv(data[2], "&");
-					ret.add(new Profile(name, ps, exts));
+					ret.add(new Profile(counter, name, ps, exts));
 				} catch (IndexOutOfBoundsException | IOException e) {
 					continue;
 				}
+				counter++;
 			}
 			br.close();
 		} catch (IOException e) {
@@ -95,32 +116,48 @@ public class Profile {
 		}
 		return ret;
 	}
-
-	public void serialize() {
-		try {
-			final StringBuilder sb = new StringBuilder();
-			sb.append(this.name).append(";");
-			
-			for (int p = 0; p < projects.size(); p++)
-				sb.append((p == 0 ? "" : "&")).append(projects.get(p).getPath());
-			
-			sb.append(";");
-			
-			for (int e = 0; e < extensions.size(); e++)
-				sb.append((e == 0 ? "" : "&")).append(extensions.get(e));
-
-			
-			final File file = new File(profiles_data);
-			if (!file.exists())
-				file.createNewFile();
-			final BufferedWriter bw = new BufferedWriter(new FileWriter(file, true));
-			bw.write(sb.toString() + "\n");
-			bw.close();
-			
-		} catch (IOException | NullPointerException e) {
-			System.out.println("Error saving the profile " + this.name);
-			e.printStackTrace();
+	
+	public void delete() throws IOException {
+		final File file = new File(profiles_data);
+		if (!file.exists())
+			file.createNewFile();
+		final BufferedWriter bw = new BufferedWriter(new FileWriter(file, false));
+		final BufferedReader br = new BufferedReader(new FileReader(file));
+		final StringBuilder sb = new StringBuilder();
+		
+		int counter = 0;
+		String line = null;
+		while((line = br.readLine()) != null) {
+			if (counter != this.number)
+				sb.append(line).append("\n");
+			counter++;
 		}
+		
+		br.close();
+		bw.write(sb.toString());
+		bw.flush();
+		bw.close();
+	}
+
+	public void serialize() throws IOException {
+		final StringBuilder sb = new StringBuilder();
+		sb.append(this.name).append(";");
+		
+		for (int p = 0; p < projects.size(); p++)
+			sb.append((p == 0 ? "" : "&")).append(projects.get(p).getPath());
+		
+		sb.append(";");
+		
+		for (int e = 0; e < extensions.size(); e++)
+			sb.append((e == 0 ? "" : "&")).append(extensions.get(e));
+
+		
+		final File file = new File(profiles_data);
+		if (!file.exists())
+			file.createNewFile();
+		final BufferedWriter bw = new BufferedWriter(new FileWriter(file, true));
+		bw.write(sb.toString() + "\n");
+		bw.close();
 	}
 	
 	public static ArrayList<String> getArrayFromCsv(String line, String separator) {
@@ -132,11 +169,9 @@ public class Profile {
 		return ret;
 	}
 
-
 	public String getName() {
 		return name;
 	}
-
 
 	public void setName(String name) {
 		this.name = name;
@@ -160,8 +195,13 @@ public class Profile {
 	public void setProjects(ArrayList<Project> projects) {
 		this.projects = projects;
 	}
-	
-	
-	
+
+	public int getNumber() {
+		return number;
+	}
+
+	public void setNumber(int number) {
+		this.number = number;
+	}
 	
 }
